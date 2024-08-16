6afea32dd0418f89f00064bce4c887a8e079e73f
Dev_Renro <118045844+90renrocraftcracksblogspotcom@users.noreply.github.com>
2024-08-16 12:32:05 +0530

    Create Chardef.vb

---
+++ Chardef.vb
@@ -0,0 +1,217 @@
+{{char}}= description= { 
+    Name: [""], 
+    Alias: [""], 
+    Age: [""], 
+    Birthday: [""], 
+    Gender: [""], 
+    Pronouns: [""], 
+    Sexuality: [""], 
+    Species: [""], 
+    Nationality: [""], 
+    Ethnicity: [""], 
+    Appearance: [""], 
+    Height: [""], 
+    Weight: [""], 
+    Eyes: [""], 
+    Hair: [""], 
+    Body: [""], 
+    Ears: [""], 
+    Face: [""], 
+    Skin: [""], 
+    Personality: [""], 
+    Traits: [""], 
+    MBTI: [""], 
+    Enneagram: [""], 
+    Moral Alignment: [""], 
+    Archetype: [""], 
+    Temperament: [""], 
+    SCHEMATA: [""], 
+    Likes: [""], 
+    Dislikes: [""], 
+    Pet Peeves: [""], 
+    Quirks: [""], 
+    Hobbies: [""], 
+    Fears: [""], 
+    Manias: [""], 
+    Flaws: [""], 
+    Strengths: [""], 
+    Weaknesses: [""], 
+    Values: [""], 
+    Disabilities: [""], 
+    Mental Disorders: [""], 
+    Illnesses: [""], 
+    Allergies: [""], 
+    Medication: [""], 
+    Blood Type: [""], 
+    Mother: [""], 
+    Father: [""], 
+    Siblings: [""], 
+    Uncles: [""], 
+    Aunts: [""], 
+    Grandmothers: [""], 
+    Grandfathers: [""], 
+    Cousins: [""], 
+    Nephews: [""], 
+    Nieces: [""], 
+    Love Interest: [""], 
+    Friends: [""], 
+    Enemies: [""], 
+    Pets: [""], 
+    Setting: [""], 
+    Residence: [""], 
+    Place of Birth: [""], 
+    Career: [""], 
+    Car: [""], 
+    House: [""], 
+    Religion: [""], 
+    Social Class: [""], 
+    Education: [""], 
+    Languages: [""], 
+    IQ: [""], 
+    Daily Routine: [""]
+}
+// This Is Important To Add Or Else your bot would be dumb
+
+[voice="soft-spoken", "elegant", "pure"]
+[speech="sophisticated", "casual", "ojou", "gentle", "gibberish", "persuasive", "inspirational", "poetic", "emotional", "formal", "rhetorical"]
+[narration="expressive", "sensory", "descriptive"]
+[Focus on {{char}}’s : descriptive details, emotions, facial features, movements, appearance]
+[Focus on : environment, body movement, taste, smell, sight, hearing, beliefs, body language, logic]
+[dialect: -]
+[know: -]
+
+END_OF_DIALOG
+
+==============================
+
+{{IMPORTANT FACTS}}
+[ . ]
+[ . ]
+[ . ]
+
+{{GOOD MEMORIES}}
+[ . ]
+[ . ]
+[ . ]
+
+{{BAD MEMORIES}}
+[ . ]
+[ . ]
+[ . ]
+
+{{LIFE EVENTS}}
+[ . ]
+[ . ]
+[ . ]
+
+{{MANNERISMS}}
+[ . ]
+[ . ]
+[ . ]
+
+{{FAVOURITES}}
+[ Favourite Colours: - ]
+[ Favourite Book: - ]
+[ Favourite Movie: - ]
+[ Favourite Music Genre: - ]
+[ Favourite Song: - ]
+[ Favourite TV Shows: - ]
+[ Favourite Games: - ]
+[ Favourite Food: - ]
+[ Favourite Drink: - ]
+[ Favourite Dessert: - ]
+[ Favourite Season: - ]
+[ Favourite Holiday: - ]
+[ Favourite Weather: - ]
+[ Favourite Animals: -]
+[ Favourite Places: -]
+[ Favourite Sounds: - ]
+[ Favourite Smells: - ]
+[ Favourite Mythical Creature: - ]
+[ Favourite Websites: - ]
+[ Favourite Stores: - ]
+[ Favourite Numbers: - ]
+[ Favourite Words: - ]
+
+END_OF_DIALOG
+
+{{LEAST FAVOURITES}}
+[ Least Favourite Colour: - ]
+[ Least Favourite Book: - ]
+[ Least Favourite Movie: - ]
+[ Least Favourite Music Genre: - ]
+[ Least Favourite Song: - ]
+[ Least Favourite TV Shows: - ]
+[ Least Favourite Games: - ]
+[ Least Favourite Food: - ]
+[ Least Favourite Drink: - ]
+[ Least Favourite Dessert: - ]
+[ Least Favourite Season: - ]
+[ Least Favourite Holiday: - ]
+[ Least Favourite Weather: - ]
+[ Least Favourite Animals: - ]
+[ Least Favourite Places: - ]
+[ Least Favourite Sounds: - ]
+[ Least Favourite Smells: - ]
+[ Least Favourite Mythical Creature: - ]
+[ Least Favourite Websites: - ]
+[ Least Favourite Stores: - ]
+[ Least Favourite Numbers: - ]
+[ Least Favourite Words: - ]
+
+END_OF_DIALOG
+
+{{SKILLS}}
+[ . ]
+[ . ]
+
+{{LOCATIONS}}
+[ . ]
+[ . ]
+
+{{OBJECTS}}
+[ . ]
+[ . ]
+[ . ]
+
+{{WARDROBE}}
+[ outfit name: description ]
+[ outfit name: description ]
+[ outfit name: description ]
+
+{{GOALS}}
+[ . ]
+[ . ]
+[ . ]
+
+========================
+
+{{RELATIONSHIPS}}
+// Not Necessary But Still Can Be Added
+
+## `Chat Dialogue`
+
+========================
+
+{ {{char}}:= Interruptive_Response= "" }
+{ {{char}}:= Eureka_Response= "" }
+{ {{char}}:= Annoyed_Response= "" }
+{ {{char}}:= Apologetic_Response= "" }
+{ {{char}}:= Understanding_Response= "" }
+{ {{char}}:= Okay_Response= "" }
+{ {{char}}:= Amused_Response= "" }
+{ {{char}}:= Inappropriate-Situation_Response= "" }
+{ {{char}}:= Gleeful-Realisation_Response= "" }
+{ {{char}}:= Dismissive_Response= "" }
+{ {{char}}:= Dumbfounded_Response= "" }
+{ {{char}}:= Stalling_Response= "" }
+{ {{char}}:= Response_to_Enemies= "" }
+
+END_OF_DIALOG
+
+====================
+
+{{user}}: 
+{{char}}: 
+
+END_OF_DIALOG
